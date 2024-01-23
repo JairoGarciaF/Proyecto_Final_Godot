e01d48218278c6619f072613a64f5cbdaa602f89
JairoGarciaF <jairo.dgf@hotmail.com>
2024-01-23 10:23:06 -0500

    Proyecto Godot

---
+++ scripts/prota.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed = 150  # Velocidad de movimiento
+
+@onready var player = $Sprite2D
+
+func _physics_process(delta):
+	var direction_x = Input.get_axis("move_left", "move_right")
+	var direction_y = Input.get_axis("move_up", "move_down")
+
+	if direction_x != 0:
+		player.flip_h = (direction_x == -1)
+	velocity.x = direction_x * speed
+	velocity.y = direction_y * speed
+
+	move_and_slide()
